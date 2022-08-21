de869ccc9ab4e32b252caf6a2f277ac1b7463119
mahnoor <mahnoor1243@gmail.com>
2022-08-22 00:52:10 +0500

    remove debug log

--- api/v1alpha1/schemalogger_conversion.go
+++ api/v1alpha1/schemalogger_conversion.go
@@ -1,6 +1,8 @@
 package v1alpha1
 
 import (
+	"fmt"
+
 	"broadcast.logger/schemalogger/api/v1beta1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
@@ -14,7 +16,7 @@ func (src *SchemaLogger) ConvertTo(dstRaw conversion.Hub) error {
 	dst.Spec.Condition = "default-condition"
 	dst.ObjectMeta = src.ObjectMeta
 	dst.Status = v1beta1.SchemaLoggerStatus(src.Status)
-	broadcastlog.Info("Converted v1alpha1 to v1beta1", dst)
+	fmt.Println(dst)
 	return nil
 }
 
@@ -24,5 +26,6 @@ func (dst *SchemaLogger) ConvertFrom(srcRaw conversion.Hub) error {
 	dst.ObjectMeta = src.ObjectMeta
 	dst.Status = SchemaLoggerStatus(src.Status)
 	broadcastlog.Info("Converted v1beta1 to v1alpha1", dst)
+	fmt.Println(dst)
 	return nil
 }
