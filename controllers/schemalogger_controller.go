d0fd1e0bb5142351cb57e80eeb02069d54eb3c9d
mahnoor <mahnoor1243@gmail.com>
2022-08-22 00:34:20 +0500

    manager role edit

--- controllers/schemalogger_controller.go
+++ controllers/schemalogger_controller.go
@@ -47,6 +47,7 @@ type SchemaLoggerReconciler struct {
 //+kubebuilder:rbac:groups=broadcast.broadcast.logger,resources=schemaloggers,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=broadcast.broadcast.logger,resources=schemaloggers/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=broadcast.broadcast.logger,resources=schemaloggers/finalizers,verbs=update
+//+kubebuilder:rbac:groups=,resources=pods,verbs=get;list;watch
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
