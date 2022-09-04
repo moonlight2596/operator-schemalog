67827670d225762ec5575429833934988ea56fe7
mahnoor <mahnoor1243@gmail.com>
2022-09-05 02:35:09 +0500

    Webhooks of all types added

--- api/v1beta1/schemalogger_webhook.go
+++ api/v1beta1/schemalogger_webhook.go
@@ -17,10 +17,22 @@ limitations under the License.
 package v1beta1
 
 import (
+	"strings"
+
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
 )
+
+/*
+type Validator interface {
+	runtime.Object
+	ValidateCreate() error
+	ValidateUpdate(old runtime.Object) error
+	ValidateDelete() error
+}*/
 
 // log is for logging in this package.
 var schemaloggerlog = logf.Log.WithName("schemalogger-resource")
@@ -33,10 +45,9 @@ func (r *SchemaLogger) SetupWebhookWithManager(mgr ctrl.Manager) error {
 }
 
 // TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-//+kubebuilder:webhook:path=/mutate-broadcast-broadcast-logger-v1beta1-schemalogger,mutating=true,failurePolicy=fail,groups=broadcast.broadcast.logger,resources=schemaloggers,verbs=create;update,versions=v1beta1,name=mbroadcast.logger,sideEffects=None,admissionReviewVersions=v1beta1
+//+kubebuilder:webhook:path=/mutate-broadcast-broadcast-logger-v1beta1-schemalogger,mutating=true,failurePolicy=fail,groups=broadcast.broadcast.logger,resources=schemaloggers,verbs=create;update,versions=v1beta1,name=mbroadcast.broadcast.logger,sideEffects=None,admissionReviewVersions=v1beta1
 
-//webhook:verbs=create;update;delete,path=/validate-batch-tutorial-kubebuilder-io-v1-cronjob,mutating=false,failurePolicy=fail,groups=broadcast.broadcast.logger,resources=cronjobs,versions=v1,name=vbroadcast.logger,sideEffects=None,admissionReviewVersions=v1
-//kubebuilder:
+//+kubebuilder:webhook:verbs=create;update;delete,path=/validate-broadcast-broadcast-logger-v1beta1-schemalogger,mutating=false,failurePolicy=fail,groups=broadcast.broadcast.logger,resources=cronjobs,versions=v1,name=vbroadcast.broadcast.logger,sideEffects=None,admissionReviewVersions=v1beta1
 
 var _ webhook.Defaulter = &SchemaLogger{}
 
@@ -46,4 +57,24 @@ func (r *SchemaLogger) Default() {
 		r.Annotations["free-rain-check"] = "FREErainValueGhazal"
 	}
 	schemaloggerlog.Info("webhook", "rain-check", r.Name)
+}
+
+var _ webhook.Validator = &SchemaLogger{}
+
+func (r *SchemaLogger) ValidateCreate() error {
+	var Err field.Error
+	schemaloggerlog.Info("Validate Creation", "name", r.Name)
+	if !(strings.Contains(r.Spec.Image, ":")) {
+		Err = *field.Invalid(field.NewPath("Spec").Child("Image"), r.Name, "Need to have tag with image")
+		return &Err
+	}
+	return nil
+}
+
+func (r *SchemaLogger) ValidateUpdate(old runtime.Object) error {
+	return nil
+}
+
+func (r *SchemaLogger) ValidateDelete() error {
+	return nil
 }
